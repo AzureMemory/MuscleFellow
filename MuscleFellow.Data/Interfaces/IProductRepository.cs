a457ff1c2af84d5d1572a7ae59f9a2105b47c444
AzureMemory <yangchenglin@live.com>
2017-03-22 13:16:51 +0800

    添加MuscleFellow 仓储层

---
+++ MuscleFellow.Data/Interfaces/IProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using MuscleFellow.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleFellow.Data.Interfaces
+{
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// 异步获取全部产品
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Product>> GetAllAsync();
+        /// <summary>
+        /// 异步向数据源添加一个商品
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task<Guid> AddAsync(Product product);
+        /// <summary>
+        /// 删除产品
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        Task DeleteAsync(Guid productID);
+        /// <summary>
+        /// 根据id获取产品
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        Task<Product> GetAsync(Guid productID);
+        /// <summary>
+        /// 根据条件和分页信息获取产品
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageCount"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetProductAsync(string filter, int pageSize, int pageCount);
+        /// <summary>
+        /// 按照修改时间倒序排列，获取产品
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetPopularProductsAsync(int count);
+        /// <summary>
+        /// 更新产品信息
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task<Guid> UpdateAsync(Product product);
+    }
+}
---
+++ MuscleFellow.Data/Respositories/ProductRespository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuscleFellow.Data.Interfaces;
+using MuscleFellow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleFellow.Data.Respositories
+{
+    public class ProductRespository : IProductRepository
+    {
+        private MuscleFellowDbcontext _dbContext;
+        public ProductRespository(MuscleFellowDbcontext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Guid> AddAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetAsync(Guid productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetPopularProductsAsync(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductAsync(string filter, int pageSize, int pageCount)
+        {
+            var results = await _dbContext.Products.Where(t => (string.IsNullOrEmpty(filter) 
+                                                                || t.ProductName.Contains(filter)
+                                                                || t.Description.Contains(filter)))
+                                                                .Select(t => new { Product = t })
+                                                                .Skip((pageSize - 1) * pageCount)
+                                                                .Take(pageSize)
+                                                                .ToListAsync();
+            return results.Select(t => t.Product);
+        }
+
+        public Task<Guid> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
